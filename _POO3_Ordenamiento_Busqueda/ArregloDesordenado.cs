7a48d6a624f1df79b92eaa247843012917d94b34
UrielTI <urielarmenta05@gmail.com>
2022-09-27 07:59:02 -0700

    Agregue el Proyecto Final de la materia Estructura de Datos
    Subi el proyecto final de la materia Estructura de datos que incluye el arreglo, lo metodos de ordenamientos y los metodos de busqueda

---
+++ _POO3_Ordenamiento_Busqueda/ArregloDesordenado.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _POO3_Ordenamiento_Busqueda
+{
+    internal class ArregloDesordenado
+    {
+        private readonly int Max;
+
+        private int Top;
+
+        private int[] Arreglo;
+
+        public ArregloDesordenado(int Max)
+        {
+            this.Max = Max;
+
+            this.Top = 0;
+
+            this.Arreglo = new int[this.Max];
+        }
+
+        public bool EstaLleno()
+        {
+            if (this.Top == this.Max) return true;
+
+            else return false;
+        }
+
+        public bool EstaVacio()
+        {
+            if (this.Top == 0) return true;
+
+            else return false;
+        }
+
+        public bool Insertar(int Dato)
+        {
+            if (!EstaLleno())
+            {
+                for (int i = 0; i < Top - 1; i++)
+                {
+                    if (this.Arreglo[i] == Dato) return false;
+                }
+
+                this.Arreglo[this.Top] = Dato;
+
+                this.Top++;
+
+                return true;
+            }
+            else return false;
+        }
+
+        public bool Vaciar()
+        {
+            this.Top = 0;
+
+            return true;
+        }
+
+        public bool Eliminar(int Dato)
+        {
+            if (!EstaVacio())
+            {
+                for (int i = 0; i <= this.Top; i++)
+                {
+                    if (this.Arreglo[i] == Dato)
+                    {
+                        for (int j = i; j < this.Top - 1; j++)
+                        {
+                            this.Arreglo[j] = this.Arreglo[j + 1];
+                        }
+                        this.Top--;
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+            else return false;
+        }
+
+        public string Mostrar()
+        {
+            string respuesta = " Top = " + this.Top + "\n\n";
+
+            if (!EstaVacio())
+            {
+                for (int i = 0; i <= this.Top - 1; i++)
+                {
+                    respuesta = respuesta + "\t [" + i + "]  ==>  " + this.Arreglo[i] + "\n";
+                }
+            }
+            else respuesta = "El arreglo esta vacio";
+
+            return respuesta;
+        }
+
+        #region BubbleSort
+        public void BubbleSort()
+        {
+            int temp;
+
+            for (int j = 0; j <= Arreglo.Length - 2; j++)
+            {
+                for (int i = 0; i <= Arreglo.Length - 2; i++)
+                {
+                    if (Arreglo[i] > Arreglo[i + 1])
+                    {
+                        temp = Arreglo[i + 1];
+                        Arreglo[i + 1] = Arreglo[i];
+                        Arreglo[i] = temp;
+
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region HeapSort
+        public void HeapSort()
+        {
+            int n = this.Arreglo.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(this.Arreglo, n, i);
+
+            for (int i = n - 1; i > 0; i--)
+            {
+     
+                int temp = this.Arreglo[0];
+                this.Arreglo[0] = this.Arreglo[i];
+                this.Arreglo[i] = temp;
+
+                heapify(this.Arreglo, i, 0);
+            }
+        }
+
+
+        void heapify(int[] arr, int n, int i)
+        {
+            int masLargo = i; 
+            int izquierdo = 2 * i + 1;
+            int derecho = 2 * i + 2;  
+
+            if (izquierdo < n && arr[izquierdo] > arr[masLargo])
+                masLargo = izquierdo;
+
+            if (derecho < n && arr[derecho] > arr[masLargo])
+                masLargo = derecho;
+
+            if (masLargo != i)
+            {
+                int swap = arr[i];
+                arr[i] = arr[masLargo];
+                arr[masLargo] = swap;
+
+                heapify(arr, n, masLargo);
+            }
+        }
+        #endregion
+
+        #region InsertSort
+        public void InsertSort()
+        {
+            int n = this.Arreglo.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int clave = this.Arreglo[i];
+                int j = i - 1;
+
+                while (j >= 0 && this.Arreglo[j] > clave)
+                {
+                    this.Arreglo[j + 1] = this.Arreglo[j];
+                    j = j - 1;
+                }
+                this.Arreglo[j + 1] = clave;
+            }
+        }
+        #endregion
+
+        #region QuickSort
+        static void swap(int[] arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        static int partition(int[] arr, int men, int may)
+        {
+            int pivot = arr[may];
+
+
+            int i = (men - 1);
+
+            for (int j = men; j <= may - 1; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+                    swap(arr, i, j);
+                }
+            }
+            swap(arr, i + 1, may);
+            return (i + 1);
+        }
+
+        static void Sort(int[] arr, int men, int may)
+        {
+            if (men < may)
+            {
+                int pi = partition(arr, men, may);
+
+                Sort(arr, men, pi - 1);
+                Sort(arr, pi + 1, may);
+            }   
+        }
+
+        public void QuickSort()
+        {
+            Sort(this.Arreglo, 0, this.Top - 1);
+        }
+        #endregion
+
+        #region PancakeSort
+        static void flip(int[] arr, int i)
+        {
+            int temp, inicio = 0;
+            while (inicio < i)
+            {
+                temp = arr[inicio];
+                arr[inicio] = arr[i];
+                arr[i] = temp;
+                inicio++;
+                i--;
+            }
+        }
+
+        static int findMax(int[] arr, int n)
+        {
+            int mi, i;
+            for (mi = 0, i = 0; i < n; ++i)
+                if (arr[i] > arr[mi])
+                    mi = i;
+
+            return mi;
+        }
+
+
+        public void PancakeSort()
+        {
+            for (int tam_act = this.Top; tam_act > 1; --tam_act)
+            {
+
+                int mi = findMax(this.Arreglo, tam_act);
+
+                if (mi != tam_act - 1)
+                {
+                    flip(this.Arreglo, mi);
+
+                    flip(this.Arreglo, tam_act - 1);
+                }
+            }
+        }
+        #endregion
+
+        #region SelectionSort
+        public void SelectionSort()
+        {
+            int n = this.Top;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (this.Arreglo[j] < this.Arreglo[min_idx])
+                        min_idx = j;
+                int temp = this.Arreglo[min_idx];
+                this.Arreglo[min_idx] = this.Arreglo[i];
+                this.Arreglo[i] = temp;
+            }
+        }
+        #endregion
+
+
+        public static int BuscarLineal(int[] arr, int x)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                    return i;
+            }
+            return -1;
+        }
+
+        public int BusquedaLinea(int dato)
+        {
+            return BuscarLineal(this.Arreglo, dato);
+        }
+
+        public int BuscarBinaria(int busqueda)
+        {
+            int izquierda = 0, derecha = this.Arreglo.Length - 1;
+            while (izquierda <= derecha)
+            {
+
+                int indiceCentral = Convert.ToInt32(Math.Floor(Convert.ToDouble(izquierda + derecha) / 2));
+                int valorCentral = this.Arreglo[indiceCentral];
+                if (valorCentral == busqueda)
+                {
+                    return indiceCentral;
+                }
+                if (busqueda < valorCentral)
+                {
+                    derecha = indiceCentral - 1;
+                }
+                else
+                {
+                    izquierda = indiceCentral + 1;
+                }
+
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ _POO3_Ordenamiento_Busqueda/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _POO3_Ordenamiento_Busqueda
+{
+    class Program
+    {
+        ArregloDesordenado Array = new ArregloDesordenado(6);
+        static void Main(string[] args)
+        {
+            new Program().Menu();
+        }
+
+        void Menu()
+        {
+            bool ejecutar = false;
+
+            for (int k = 0; ;)
+            {
+                PintaMenu(k);
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+
+                switch (cki.Key)
+                {
+                    case ConsoleKey.UpArrow: k--; break;
+                    case ConsoleKey.DownArrow: k++; break;
+                    case ConsoleKey.Enter: ejecutar = true; break;
+                }
+
+                if (k < 0) k = 8; else if (k > 8) k = 0;
+
+                if (ejecutar)
+                {
+                    ejecutar = false;
+                    switch (k)
+                    {
+                        case 0: Opcion1(); break;
+                        case 1: Opcion2(); break;
+                        case 2: Opcion3(); break;
+                        case 3: Opcion4(); break;
+                        case 4: Opcion5(); break;
+                        case 5: Opcion6(); break;
+                        case 6: Opcion7(); break;
+                        case 7: Opcion8(); break;
+                        case 8: return;
+                    }
+                }
+            }
+        }
+
+        void PintaMenu(int k)
+        {
+            ConsoleColor cc = ConsoleColor.White;
+            ConsoleColor sel = ConsoleColor.Red;
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(5, 5);
+            Console.ForegroundColor = k == 0 ? sel : cc;
+            Console.WriteLine("1. Precargar el arreglo con {7,3,9,4,6,5}");
+            Console.SetCursorPosition(5, 7);
+            Console.ForegroundColor = k == 1 ? sel : cc;
+            Console.WriteLine("2. Insertar dato en el arreglo");
+            Console.SetCursorPosition(5, 9);
+            Console.ForegroundColor = k == 2 ? sel : cc;
+            Console.WriteLine("3. Eliminar dato del arreglo");
+            Console.SetCursorPosition(5, 11);
+            Console.ForegroundColor = k == 3 ? sel : cc;
+            Console.WriteLine("4. Vaciar el arreglo");
+            Console.SetCursorPosition(5, 13);
+            Console.ForegroundColor = k == 4 ? sel : cc;
+            Console.WriteLine("5. Mostrar el arreglo");
+            Console.SetCursorPosition(5, 15);
+            Console.ForegroundColor = k == 5 ? sel : cc;
+            Console.WriteLine("6.  Ordenar el arreglo");
+            Console.SetCursorPosition(5, 17);
+            Console.ForegroundColor = k == 6 ? sel : cc;
+            Console.WriteLine("7. Buscar dato en el arreglo con el metodo de busqueda lineal");
+            Console.SetCursorPosition(5, 19);
+            Console.ForegroundColor = k == 7 ? sel : cc;
+            Console.WriteLine("8. Buscar dato en el arreglo con el metodo de busqueda binaria");
+            Console.SetCursorPosition(5, 21);
+            Console.ForegroundColor = k == 8 ? sel : cc;
+            Console.WriteLine("9. Salir");
+        }
+
+        void Opcion1()
+        {
+            Console.SetCursorPosition(5, 25);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Precargando el Arreglo");
+            Array.Insertar(7);
+            Array.Insertar(3);
+            Array.Insertar(9);
+            Array.Insertar(4);
+            Array.Insertar(6);
+            Array.Insertar(5);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        void Opcion2()
+        {
+            Console.SetCursorPosition(5, 25);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ingresae un dato para insertar al arreglo");
+            Console.SetCursorPosition(5, 26);
+            int dato = int.Parse(Console.ReadLine());
+            Array.Insertar(dato);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        void Opcion3()
+        {
+            Console.SetCursorPosition(5, 25);
+            System.Console.WriteLine("Ingrese el dato que desea eliminar en el arreglo: ");
+            Console.SetCursorPosition(5, 26);
+            int dato = int.Parse(Console.ReadLine());
+            Array.Eliminar(dato);
+            Console.Clear();
+        }
+
+        void Opcion4()
+        {
+            Array.Vaciar();
+            Console.Clear();
+        }
+
+        void Opcion5()
+        {
+            Console.Clear();
+            System.Console.WriteLine(Array.Mostrar());
+            Console.ReadKey();
+
+        }
+
+        void Opcion6()
+        {
+
+            Console.WriteLine("Seleccione el algoritmo de ordenamiento");
+            Console.WriteLine("\t1: BubbleSort");
+            System.Console.WriteLine("\t2: HeapSort");
+            System.Console.WriteLine("\t3: InsertSort");
+            System.Console.WriteLine("\t4: QuickSort");
+            System.Console.WriteLine("\t5: PancakeSort");
+            System.Console.WriteLine("\t6: SelectionSort");
+            System.Console.WriteLine("\t7: Regresar al menu principal");
+            System.Console.Write("\tOpcion: ");
+            int opc = int.Parse(Console.ReadLine());
+            switch (opc)
+            {
+                case 1:
+                    Array.BubbleSort();
+                    break;
+
+                case 2:
+                    Array.HeapSort();
+                    break;
+
+                case 3:
+                    Array.InsertSort();
+                    break;
+
+                case 4:
+                    Array.QuickSort();
+                    break;
+
+                case 5:
+                    Array.PancakeSort();
+                    break;
+
+                case 6:
+                    Array.SelectionSort();
+                    break;
+            }
+        }
+
+        void Opcion7()
+        {
+            Console.WriteLine("Ingrese el dato a buscar en el arrreglo por busqueda lineal");
+            int dato = int.Parse(Console.ReadLine());
+            Console.WriteLine("El dato "+dato+" se encuentra en la posicion " +Array.BusquedaLinea(dato));
+            Console.ReadKey();
+        }
+
+        void Opcion8()
+        {
+            Console.WriteLine("Ingrese el dato a buscar en el arrreglo por busqueda binaria");
+            int dato = int.Parse(Console.ReadLine());
+            int posicion = Array.BuscarBinaria(dato);
+            Console.WriteLine("El dato se encuentra en la posicion "+ posicion);
+            Console.ReadKey();
+        }
+    }
+}
